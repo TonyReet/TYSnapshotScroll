bcb63aa7544a3952f6a8a6a8031d44d59131d917
TonyReet <20130639@qq.com>
2017-12-11 14:30:50 +0800

    参考"SwViewCapture"修复WKWebView在10.1.1上截取有问题的BUG

--- TYSnapshotScroll/TYSnapshot/WKWebView+TYSnapshot.m
+++ TYSnapshotScroll/TYSnapshot/WKWebView+TYSnapshot.m
@@ -11,23 +11,32 @@
 @implementation WKWebView (TYSnapshot)
 
 - (void )screenSnapshot:(void(^)(UIImage *snapShotImage))finishBlock{
-    //保存原始信息
-    CGPoint oldOffset = self.scrollView.contentOffset;
-    CGRect oldFrame = self.frame;
-    
-    self.frame = CGRectMake(0, 0, oldFrame.size.width, self.scrollView.contentSize.height);
+    //添加遮盖
+    UIView *snapShotView = [self snapshotViewAfterScreenUpdates:YES];
+    snapShotView.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, snapShotView.frame.size.width, snapShotView.frame.size.height);
+    [self.superview addSubview:snapShotView];
 
-    //是否有余数，如果有余数需要增加一个屏幕数
-    NSUInteger isRemainder = ((NSUInteger)self.scrollView.contentSize.height % (NSUInteger)self.scrollView.bounds.size.height)?1:0;
+    
+    //保存原始信息
+    CGRect oldFrame = self.frame;
+    CGPoint oldOffset = self.scrollView.contentOffset;
+    CGSize contentSize = self.scrollView.contentSize;
     
     //计算快照屏幕数
-    NSUInteger snapshotScreenCount = self.scrollView.contentSize.height / self.scrollView.bounds.size.height + isRemainder;
+    NSUInteger snapshotScreenCount = floorf(contentSize.height / self.scrollView.bounds.size.height);
     
-    UIGraphicsBeginImageContextWithOptions(self.scrollView.contentSize, NO, [UIScreen mainScreen].scale);
+    //设置frame为contentSize
+    self.frame = CGRectMake(0, 0, contentSize.width, contentSize.height);
+
+    self.scrollView.contentOffset = CGPointZero;
+    
+    UIGraphicsBeginImageContextWithOptions(contentSize, NO, [UIScreen mainScreen].scale);
     
     __weak typeof(self) weakSelf = self;
     //截取完所有图片
     [self scrollToDraw:0 maxIndex:(NSInteger )snapshotScreenCount finishBlock:^{
+        [snapShotView removeFromSuperview];
+        
         UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
         
@@ -42,15 +51,19 @@
 
 //滑动画了再截图
 - (void )scrollToDraw:(NSInteger )index maxIndex:(NSInteger )maxIndex finishBlock:(void(^)())finishBlock{
-    
-    self.scrollView.contentOffset = CGPointMake(0, (float)index * self.scrollView.frame.size.height);
+    UIView *superView = self.superview;
     
     //截取的frame
-    CGRect snapshotFrame = CGRectMake(0, (float)index * self.scrollView.frame.size.height, self.bounds.size.width, self.bounds.size.height);
+    CGRect snapshotFrame = CGRectMake(0, (float)index * superView.bounds.size.height, superView.bounds.size.width, superView.bounds.size.height);
 
+    // set up webview originY
+    CGRect myFrame = self.frame;
+    myFrame.origin.y = -((index) * superView.frame.size.height);
+    self.frame = myFrame;
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(300 * NSEC_PER_MSEC)), dispatch_get_main_queue(), ^{
         
-        [self drawViewHierarchyInRect:snapshotFrame afterScreenUpdates:YES];
+        [superView drawViewHierarchyInRect:snapshotFrame afterScreenUpdates:YES];
         
         if(index < maxIndex){
             [self scrollToDraw:index + 1 maxIndex:maxIndex finishBlock:finishBlock];
