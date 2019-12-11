164493a925fdc998ef28c06831ec9225dcfe27c2
任涛 <ktonyreet@gmail.com>
2019-12-11 16:05:13 +0800

    修改获取的图层，避免有些设备截图错误

--- TYSnapshotScroll/UIViewController+TYSnapshot.m
+++ TYSnapshotScroll/UIViewController+TYSnapshot.m
@@ -14,15 +14,18 @@
     UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
     UIViewController *vc = keyWindow.rootViewController;
     
-    do {
+    while (vc.presentedViewController) {
+        vc = vc.presentedViewController;
+        
         if ([vc isKindOfClass:[UINavigationController class]]) {
             vc = [(UINavigationController *)vc visibleViewController];
         } else if ([vc isKindOfClass:[UITabBarController class]]) {
             vc = [(UITabBarController *)vc selectedViewController];
         }
-    } while (vc.presentedViewController);
-
+    }
+    
     return vc;
 }
+
 
 @end
