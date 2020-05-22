f6c3f3ac840092b19a6dd62c5194c802db2514e4
任涛 <ktonyreet@gmail.com>
2020-05-22 17:26:12 +0800

    更改UITextView，更改版本为0.2.2

---
+++ TYSnapshotScroll/UITextView+TYSnapshot.m
@@ -0,0 +1,93 @@
+//
+//  UITextView+TYSnapshot.m
+//  Pods-TYSnapshotScrollDemo
+//
+//  Created by TonyReet on 2020/5/22.
+//
+
+#import "UITextView+TYSnapshot.h"
+#import "TYGCDTools.h"
+#import "UIView+TYSnapshot.h"
+#import "TYSnapshotManager.h"
+#import <TYSnapshotAuxiliary/UIScrollView+TYSnapshotAuxiliary.h>
+
+@implementation UITextView (TYSnapshot)
+
+- (void )screenSnapshotNeedMask:(BOOL)needMask addMaskAfterBlock:(void(^)(void))addMaskAfterBlock finishBlock:(TYSnapshotFinishBlock )finishBlock{
+    if (!finishBlock)return;
+    
+    UIView *snapshotMaskView;
+    if (needMask){
+        snapshotMaskView = [self addSnapshotMaskView];
+        addMaskAfterBlock?addMaskAfterBlock():nil;
+    }
+    
+    //保存原始信息
+    __block CGPoint oldContentOffset;
+    __block CGSize contentSize;
+    
+    onMainThreadSync(^{
+        oldContentOffset = self.contentOffset;
+        contentSize = self.contentSize;
+    });
+    
+
+    [self snapshotNormalImageWith:snapshotMaskView contentSize:contentSize oldContentOffset:oldContentOffset finishBlock:finishBlock];
+}
+
+- (BOOL )isBigImageWith:(CGSize )contentSize{
+    TYSnapshotManager *snapshotManager = [TYSnapshotManager defaultManager];
+    
+    if (contentSize.width * contentSize.height > snapshotManager.maxImageSize){
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (void )snapshotNormalImageWith:(UIView *)snapshotMaskView contentSize:(CGSize )contentSize oldContentOffset:(CGPoint )oldContentOffset finishBlock:(TYSnapshotFinishBlock )finishBlock{
+    //保存frame
+    __block CGRect oldFrame;
+    
+    onMainThreadSync(^{
+        oldFrame = self.layer.frame;
+        // 划到bottom
+        if (self.contentSize.height > self.frame.size.height) {
+            self.contentOffset = CGPointMake(0, self.contentSize.height - self.bounds.size.height + self.contentInset.bottom);
+        }
+        
+        self.layer.frame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+    });
+    
+    CGFloat delayTime = [TYSnapshotManager defaultManager].delayTime;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayTime * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIImage* snapshotImage = nil;
+        
+        self.contentOffset = CGPointZero;
+        
+        UIGraphicsBeginImageContextWithOptions(self.layer.frame.size, NO, [UIScreen mainScreen].scale);
+        
+        if (@available(iOS 13.0, *)) {
+            [self.attributedText drawInRect:self.layer.frame];
+        } else {
+            CGContextRef context = UIGraphicsGetCurrentContext();
+            [self.layer renderInContext:context];
+        }
+
+        snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+        //还原
+        self.layer.frame = oldFrame;
+        self.contentOffset = oldContentOffset;
+        
+        if (snapshotMaskView.layer){
+            [snapshotMaskView.layer removeFromSuperlayer];
+        }
+        
+        !finishBlock?:finishBlock(snapshotImage);
+    });
+}
+
+@end
